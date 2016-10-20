76157a0821730ceceeee616772c111c0fbe23224
tossmann <ptrautma@XIN842949.asv.local>
2016-10-20 15:53:05 +0200

    created project

---
+++ crypto_Ue1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  crypto_Ue1
+//
+//  Created by Trautmann, Phillip on 20.10.16.
+//  Copyright © 2016 Trautmann, Phillip. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
