724d62231dd27eabc1be02f008fe938e82ffcc9e
AndreyDelany <dielanie@freenet.de>
2016-10-20 18:37:18 +0200

    implemented decrypting, but working not hunderprocent right

--- crypto_Ue1/main.swift
+++ crypto_Ue1/main.swift
@@ -8,42 +8,81 @@
 
 import Foundation
 //Zwischen Buchstaben eine Leerstelle zwischen Wörtern zwei
+
 var morseCodeTable: [Character: String] = ["A":".-",
-                                       "B":"-...",
-                                       "C":"-.-.",
-                                       "D":"-..",
-                                       "E":".",
-                                       "F":"..-.",
-                                       "G":"--.",
-                                       "H":"....",
-                                       "I":"..",
-                                       "J":".---",
-                                       "K":"-.-",
-                                       "L":".-..",
-                                       "M":"--",
-                                       "N":"-.",
-                                       "O":"---",
-                                       "P":".--.",
-                                       "Q":"--.-",
-                                       "R":".-.",
-                                       "S":"...",
-                                       "T":"-",
-                                       "U":"..-",
-                                       "V":"...-",
-                                       "W":".--",
-                                       "X":"-..-",
-                                       "Y":"-.--",
-                                       "Z":"--..",
-                                       "1":".----",
-                                       "2":"..---",
-                                       "3":"...--",
-                                       "4":"....-",
-                                       "5":".....",
-                                       "6":"-....",
-                                       "7":"--...",
-                                       "8":"---..",
-                                       "9":"----.",
-                                       "0":"-----",
+                                           "B":"-...",
+                                           "C":"-.-.",
+                                           "D":"-..",
+                                           "E":".",
+                                           "F":"..-.",
+                                           "G":"--.",
+                                           "H":"....",
+                                           "I":"..",
+                                           "J":".---",
+                                           "K":"-.-",
+                                           "L":".-..",
+                                           "M":"--",
+                                           "N":"-.",
+                                           "O":"---",
+                                           "P":".--.",
+                                           "Q":"--.-",
+                                           "R":".-.",
+                                           "S":"...",
+                                           "T":"-",
+                                           "U":"..-",
+                                           "V":"...-",
+                                           "W":".--",
+                                           "X":"-..-",
+                                           "Y":"-.--",
+                                           "Z":"--..",
+                                           "1":".----",
+                                           "2":"..---",
+                                           "3":"...--",
+                                           "4":"....-",
+                                           "5":".....",
+                                           "6":"-....",
+                                           "7":"--...",
+                                           "8":"---..",
+                                           "9":"----.",
+                                           "0":"-----",
+                                           " ":" "]
+
+var morseCodeTableReverse: [String: Character] = [".-":"A",
+                                       "-...":"B",
+                                       "-.-.":"C",
+                                       "-..":"D",
+                                       ".":"E",
+                                       "..-.":"F",
+                                       "--.":"G",
+                                       "....":"H",
+                                       "..":"I",
+                                       ".---":"J",
+                                       "-.-":"K",
+                                       ".-..":"L",
+                                       "--":"M",
+                                       "-.":"N",
+                                       "---":"O",
+                                       ".--.":"P",
+                                       "--.-":"Q",
+                                       ".-.":"R",
+                                       "...":"S",
+                                       "-":"T",
+                                       "..-":"U",
+                                       "...-":"V",
+                                       ".--":"W",
+                                       "-..-":"X",
+                                       "-.--":"Y",
+                                       "--..":"Z",
+                                       ".----":"1",
+                                       "..---":"2",
+                                       "...--":"3",
+                                       "....-":"4",
+                                       ".....":"5",
+                                       "-....":"6",
+                                       "--...":"7",
+                                       "---..":"8",
+                                       "----.":"9",
+                                       "-----":"0",
                                        " ":" "]
 
 
@@ -65,6 +104,13 @@ func translateToMorse(symbol: Character) -> String{
     return "???"
 }
 
+func translateOneLetter(symbol: String) -> Character {
+    if let symbol = morseCodeTableReverse[symbol]{
+        return symbol
+    }
+    return "?"
+}
+
 func translateLettersToMorseCode(_ inputString: String) -> String {
     var resultString = ""
     for index in inputString.characters.indices {
@@ -73,6 +119,18 @@ func translateLettersToMorseCode(_ inputString: String) -> String {
     return resultString
 }
 
+func translateMorseCodeToLetters(_ inputString: String) -> String {
+    var resultString = ""
+    var words = inputString.components(separatedBy: "  ")
+    for currentWord in words {
+        var componentsOfFirstWord = currentWord.components(separatedBy: " ")
+        for currentLetter in componentsOfFirstWord {
+            resultString.append(translateOneLetter(symbol:currentLetter))
+        }
+        resultString.append(" ")
+    }
+    return resultString
+}
 
 func encodeMesageToMorseCode () -> String {
     let inputFromCommandLine = getInputStringFromCommandLine()
@@ -80,4 +138,11 @@ func encodeMesageToMorseCode () -> String {
     return encodedMessage
 }
 
-print(encodeMesageToMorseCode())
+func decodeSecretMessage () -> String{
+    let inputFromCommandLine = getInputStringFromCommandLine()
+    let decodeMessage = translateMorseCodeToLetters(inputFromCommandLine)
+    return decodeMessage
+}
+
+//print(encodeMesageToMorseCode())
+print(decodeSecretMessage())
